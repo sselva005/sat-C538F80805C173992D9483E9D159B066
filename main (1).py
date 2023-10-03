23d0d5cbab4b6d70fb5b1b515503e77eb744b8aa
sselva005 <146044094+sselva005@users.noreply.github.com>
2023-10-03 14:55:44 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,9 +1,35 @@
-Certainly! Here's a Pyt
-# Get the year from the user
-year = int(input("Enter a year: "))
+Certainly! Here's a Python implementation of the `Player`, `Batsman`, and `Bowler` classes, along with a program to create objects of each class and call their `play()` methods:
 
-# Check if it's a leap year
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year, "is a leap year.")
-else:
-    print(year, "is not a leap year.")
+```python
+# Define the Player class
+class Player:
+    def play(self):
+        print("The player is playing cricket")
+
+# Define the Batsman class, derived from Player
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting")
+
+# Define the Bowler class, derived from Player
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling")
+
+# Create objects of the Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
+```
+
+When you run this program, it will create objects of the `Batsman` and `Bowler` classes and call their `play()` methods, producing the following output:
+
+```
+The batsman is batting
+The bowler is bowling
+```
+
+This demonstrates the concept of inheritance and method overriding in object-oriented programming, where the derived classes (`Batsman` and `Bowler`) override the `play()` method from the base class (`Player`) to provide their specific behavior.
